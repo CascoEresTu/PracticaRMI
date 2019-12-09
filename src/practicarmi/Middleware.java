64923fd0c7c4ff7d63d1de12a8463ef3927189d1
CascoEresTu <k_k_ryuten@live.com>
2019-12-08 20:54:47 -0600

    El crud en teoria deberia funcionar. Se ocupa al menos un client para probar

--- src/practicarmi/Middleware.java
+++ src/practicarmi/Middleware.java
@@ -20,64 +20,90 @@ import javax.swing.tree.DefaultTreeModel;
  */
 public class Middleware extends UnicastRemoteObject implements Service {
 
-    /*
-        ArrayList clientes = new ArrayList();
-        String name; // para identificar de manera mas sencilla al cliente
+    /*  
+    TODO:
+        ArrayList clientes = new ArrayList(); // Creo que tendremos varias instancias de clientes en un arraylist, para poder desmontarlas
+        String name; // para identificar de manera mas sencilla al cliente y al servidor
         Cliente // creo que esta sera cada invocacion del GUI del cliente.
      */
+    public String name;
 
-    @Override
-    public void CrearArchivo(File archivo, boolean tipo) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Middleware(String name) throws RemoteException {
+        this.name = "Servidor";
+    }
+
+    public Middleware(String name, Object cliente) throws RemoteException {
+        this.name = "Servidor";
+        //this.cliente = cliente;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
-    public void EditarArchivo(File archivo) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void EliminarArchivo(File archivo, boolean tipo) throws RemoteException{
+    public void CrearArchivo(File archivo, boolean tipo) throws RemoteException, IOException {
         File temp = PathServer(archivo);
-        this.eliminarDirectorios(archivo);
-        
+        if (tipo) {
+            temp.getParentFile().mkdirs();
+            temp.createNewFile();
+        } else {
+            temp.mkdirs();
+        }
+    }
+
+    @Override
+    public void EditarArchivo(File archivo, String Texto) throws RemoteException {
+        try {
+            Files.write(Paths.get(archivo.getAbsolutePath()), Texto.getBytes());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    @Override
+    public void EliminarArchivo(File archivo, boolean tipo) throws RemoteException {
+        File temp = PathServer(archivo);
+        this.eliminarDirectorios(temp);
+
     }
 
     @Override
     public DefaultTreeModel CargarDirectorio() throws RemoteException {
-        DefaultMutableTreeNode raiz = new DefaultMutableTreeNode(new Archivo (new File("RootServer"),"",false));
-        cargarArbol("RootServer",raiz);
+        DefaultMutableTreeNode raiz = new DefaultMutableTreeNode(new Archivo(new File("RootServer"), "", false));
+        cargarArbol("RootServer", raiz);
         return new DefaultTreeModel(raiz);
-    
+
     }
 
     @Override
     public void Desmontar(String cliente) throws RemoteException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    public File PathServer(File archivo){
+
+    public File PathServer(File archivo) {
         String path = archivo.getName();
         File parent = archivo;
-        
-       /* 
+
+        /* 
         while (true) {
             parent = parent.getParentFile();
             if (parent.getName().equals("RootServer")) {
                 break;
             }            
         } */
-        while(!parent.getName().equals("RootServer")){
+        while (!parent.getName().equals("RootServer")) {
             parent = parent.getParentFile();
         }
-        
-        
+
         path = parent.getName() + "/" + path;
         return new File(path);
     }
-    
-    
-    
 
     public void cargarArbol(String directorio, DefaultMutableTreeNode node) {
         File raiz = new File(directorio);
@@ -94,14 +120,13 @@ public class Middleware extends UnicastRemoteObject implements Service {
                 temp = new Archivo(file, "", false);
                 DefaultMutableTreeNode subDirectorio = new DefaultMutableTreeNode(temp);
                 File subFile = new File(raiz.getAbsolutePath(), nombre);
-                cargarArbol(subFile.getAbsolutePath(),subDirectorio);
+                cargarArbol(subFile.getAbsolutePath(), subDirectorio);
                 node.add(subDirectorio);
             }
         }
     }
-   
-    
-    public void eliminarDirectorios(File root){
+
+    public void eliminarDirectorios(File root) {
         File[] allContents = root.listFiles();
         if (allContents != null) {
             for (File file : allContents) {
@@ -110,7 +135,7 @@ public class Middleware extends UnicastRemoteObject implements Service {
         }
         root.delete();
     }
-    
+
     private String leerArchivo(String nombreArchivo) {
         String texto = "";
         try {
